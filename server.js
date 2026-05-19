739405526daa9a69497f9a7e0ebc5a634cea4a8c
Senitha Velmurugan <senithavelmurugan1125@gmail.com>
2026-05-20 00:03:44 +0530

    db

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Anonymous Thoughts Backend Running");
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
 });
 
-app.post("/message", (req, res) => {
-
-    console.log(req.body);
-
-    res.send("Message received");
+app.get("/", (req, res) => {
+    res.send("Anonymous Backend Running");
 });
 
 const PORT = 5000;
