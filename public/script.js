ed7f17f927e2120b980dc23580bb1987beb2db61
Senitha Velmurugan <senithavelmurugan1125@gmail.com>
2026-05-21 23:19:08 +0530

    login ui

--- public/script.js
+++ public/script.js
@@ -1,3 +1,32 @@
+async function loginUser() {
+
+    const username = document.getElementById("username").value;
+
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("/login", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            username,
+            password
+        })
+
+    });
+
+    const data = await response.json();
+
+    localStorage.setItem("token", data.token);
+
+    alert("Login successful");
+
+}
+
 async function sendMessage() {
 
     const message = document.getElementById("messageInput").value;
@@ -25,7 +54,15 @@ async function sendMessage() {
 
 async function loadMessages() {
 
-    const response = await fetch("/messages");
+    const token = localStorage.getItem("token");
+
+const response = await fetch("/messages", {
+
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+
+});
 
     const messages = await response.json();
 
